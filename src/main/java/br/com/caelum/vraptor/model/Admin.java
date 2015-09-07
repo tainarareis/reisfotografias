21d3731c64da8a4bdad5da911c8d8afd2cdadd5d
Tainara Santos Reis <tainara.s.r@hotmail.com>
2015-09-06 23:19:09 -0300

    Creating the admin registration

---
+++ src/main/java/br/com/caelum/vraptor/model/Admin.java
@@ -0,0 +1,45 @@
+package br.com.caelum.vraptor.model;
+
+public class Admin {
+
+	
+	private long id;
+	private String name;
+	private String login;
+	private String email;
+	private String password;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
